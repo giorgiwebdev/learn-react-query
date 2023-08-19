24b2c5391699b9733ce63b76370208c79deef5f7
giorgi rapava <giorgirapava.webdev@gmail.com>
2023-08-19 18:21:23 +0400

    add Manual refetching

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import {
   QueryClient,
+  useQueryClient,
   QueryCache,
   MutationCache,
   QueryClientProvider,
@@ -96,6 +97,19 @@ function App() {
     refetchInterval: 2000, //refetch every 2 seconds
     refetchIntervalInBackground: true, //keep refetching, even if your window or tab is in the background. ( "Keep checking for updates, even if you're not looking at the weather.")
   });
+
+  //Manual refetching using QueryClient
+  const queryClientHook = useQueryClient(); //Using the useQueryClient hook to get access to our QueryClient.
+  queryClientHook.refetchQueries({ queryKey: ["api"] }); //we are triggering a request for all queries that have the ["api"] query key.
+
+  //Manual refetching using refetch function
+  const { refetch } = useQuery({
+    queryKey: ["api"],
+    queryFn: fetchData,
+  });
+  //This function will allow you to trigger a refetch for just that query.
+  ////we can call that function whenever we want to force that query to refetch.
+  refetch();
 
   return (
     <QueryClientProvider client={queryClient}>
