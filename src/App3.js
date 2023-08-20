141b09733345422e933475749838959be09b99b1
giorgi rapava <giorgirapava.webdev@gmail.com>
2023-08-20 11:26:27 +0400

    create dynamic parallel queries

--- src/App3.js
+++ src/App3.js
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueries } from "@tanstack/react-query";
 
 const fetchData = () => [];
 
+//Manual parallel queries
 const ExampleOne = () => {
   const { data: queryOneData } = useQuery({
     queryKey: [{ queryIdentifier: "api", username: "userOne" }],
@@ -20,6 +21,27 @@ const ExampleOne = () => {
       <p>{queryOneData?.hello}</p>
       <p>{queryTwoData?.hello}</p>
       <p>{queryThreeData?.hello}</p>
+    </div>
+  );
+};
+
+//Dynamic parallel queries
+const usernameList = ["userOne", "userTwo", "userThree"];
+
+const ExampleTwo = () => {
+  const multipleQueries = useQueries({
+    queries: usernameList.map((username) => {
+      return {
+        queryKey: [{ queryIdentifier: "api", username }],
+        queryFn: fetchData,
+      };
+    }),
+  });
+  return (
+    <div>
+      {multipleQueries.map(({ data, isFetching }) => (
+        <p>{isFetching ? "Fetching data..." : data.hello}</p>
+      ))}
     </div>
   );
 };
