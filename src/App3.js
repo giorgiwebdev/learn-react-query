0c0df6c438ae15cdde5b0a8c28ac69f7b6932ba4
giorgi rapava <giorgirapava.webdev@gmail.com>
2023-08-22 21:47:22 +0400

    update: parallel queries manual & dynamic

--- src/App3.js
+++ src/App3.js
@@ -4,50 +4,6 @@ import { useQuery, useQueries, useQueryClient } from "@tanstack/react-query";
 
 // const fetchData = () => [];
 
-// //Manual parallel queries
-// const ExampleOne = () => {
-//   const { data: queryOneData } = useQuery({
-//     queryKey: [{ queryIdentifier: "api", username: "userOne" }],
-//     queryFn: fetchData,
-//   });
-//   const { data: queryTwoData } = useQuery({
-//     queryKey: [{ queryIdentifier: "api", username: "userTwo" }],
-//     queryFn: fetchData,
-//   });
-//   const { data: queryThreeData } = useQuery({
-//     queryKey: [{ queryIdentifier: "api", username: "userThree" }],
-//     queryFn: fetchData,
-//   });
-//   return (
-//     <div>
-//       <p>{queryOneData?.hello}</p>
-//       <p>{queryTwoData?.hello}</p>
-//       <p>{queryThreeData?.hello}</p>
-//     </div>
-//   );
-// };
-
-// //Dynamic parallel queries
-// const usernameList = ["userOne", "userTwo", "userThree"];
-
-// const ExampleTwo = () => {
-//   const multipleQueries = useQueries({
-//     queries: usernameList.map((username) => {
-//       return {
-//         queryKey: [{ queryIdentifier: "api", username }],
-//         queryFn: fetchData,
-//       };
-//     }),
-//   });
-//   return (
-//     <div>
-//       {multipleQueries.map(({ data, isFetching }) => (
-//         <p>{isFetching ? "Fetching data..." : data.hello}</p>
-//       ))}
-//     </div>
-//   );
-// };
-
 // const ExampleThree = () => {
 //   const queryClient = useQueryClient();
 
