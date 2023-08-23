e0f73b3f1544bde065deb326ff1de1bce2c17438
giorgi rapava <giorgirapava.webdev@gmail.com>
2023-08-23 23:56:15 +0400

    update: add refetching queries automatic & manual

--- src/App.js
+++ src/App.js
@@ -90,29 +90,7 @@ function App() {
     enabled: arrayVariable.length > 0, //default: true
     onSuccess: (data) => console.log("query was successful", data),
     onError: (error) => console.log("query was unsuccessful", error.message),
-
-    //options related to data refetching
-    refetchOnMount: "always", // refetch even if the cached data is not stale (whenever any component using it mounts)
-    refetchOnReconnect: true, // refetch only if our data is stale(not fresh) (after regain connection after being offlin)
-    refetchOnWindowFocus: false, // never refetch (on window focus)
-
-    //force our hook to refetch
-    refetchInterval: 2000, //refetch every 2 seconds
-    refetchIntervalInBackground: true, //keep refetching, even if your window or tab is in the background. ( "Keep checking for updates, even if you're not looking at the weather.")
   });
-
-  //Manual refetching using QueryClient
-  const queryClientHook = useQueryClient(); //Using the useQueryClient hook to get access to our QueryClient.
-  queryClientHook.refetchQueries({ queryKey: ["api"] }); //we are triggering a request for all queries that have the ["api"] query key.
-
-  //Manual refetching using refetch function
-  const { refetch } = useQuery({
-    queryKey: ["api"],
-    queryFn: fetchData,
-  });
-  //This function will allow you to trigger a refetch for just that query.
-  ////we can call that function whenever we want to force that query to refetch.
-  refetch();
 
   //Fetching dependent queries with useQuery, enabled option.
   const { data: firstQueryData } = useQuery({
