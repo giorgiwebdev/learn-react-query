13421ea19791d982115eca502ba94d80a33baca6
giorgi rapava <giorgirapava.webdev@gmail.com>
2023-08-24 18:57:49 +0400

    A practical example of how information is updated on the server by calling the mutation function

--- src/AppUseMutationReturns.js
+++ src/AppUseMutationReturns.js
@@ -1,10 +1,44 @@
-import { useMutation } from "@tanstack/react-query";
-import React from "react";
+// import { useMutation } from "@tanstack/react-query";
+// import React from "react";
 
-function AppUseMutationReturns() {
-  const { mutate, mutateAsync, data, error, reset, status, isPaused } =
-    useMutation({
-      mutationFn: async () => {},
-    });
-  return <div></div>;
-}
+// function AppUseMutationReturns() {
+//   const { mutate, mutateAsync, data, error, reset, status, isPaused } =
+//     useMutation({
+//       mutationFn: async () => {},
+//     });
+//   return <div></div>;
+// }
+
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
+const createUser = async (user) => {
+  return axios.post(`https://danieljcafonso.builtwithdark.com/name-api`, user);
+};
+
+const SimpleMutation = () => {
+  const [name, setName] = useState("");
+  const { mutate } = useMutation({
+    mutationFn: createUser,
+  });
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    mutate({ name });
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          name='name'
+          type={"text"}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button onClick={submitForm}>Add</button>
+      </form>
+    </div>
+  );
+};
